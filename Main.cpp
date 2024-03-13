17572a33cd5e591a3a7723e8d252725077da60a3
TheCasualDev <thecasualdev@proton.me>
2024-03-13 16:30:30 +1100

    HELLO TRIANGLE - created a vertex & fragment shader - created compilers for said shaders - created a linker to link the shaders on render - I did something vertexs, but I still don't really get them - render a triagnle

--- Main.cpp
+++ Main.cpp
@@ -6,8 +6,26 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // window config
-const unsigned int WIND_WIDTH = 800;
+const unsigned int WIND_WIDTH = 600;
 const unsigned int WIND_HEIGHT = 600;
+
+// shader data
+
+const char *vertexShaderSource = 
+"#version 400 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+"}\0";
+
+const char *fragmentShaderSource = 
+"#version 400 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(0.9686f, 0.5f, 0.5098f, 0.1373f);"
+"}\0";
 
 int main()
 {
@@ -35,12 +53,94 @@ int main()
 
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	
+
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) // Checks that GLAD is working I hope.
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+
+	unsigned int vertexShader;
+
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);            // Pass the kind of shader we wish to create
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); // Sets the source of the shader to the defined above shader code
+	glCompileShader(vertexShader);								// Complies the shader
+
+	// vertexShaderCompiler Checker / debugger
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	unsigned int fragmentShader;
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); 
+	glCompileShader(fragmentShader);
+
+	// fragmentShaderCompiler Checker / debugger
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// shader program 
+
+	unsigned int shaderProgram;
+
+	shaderProgram = glCreateProgram();			   // creates a shader program
+	glAttachShader(shaderProgram, vertexShader);   // attatches the vertex shader to the shader program
+	glAttachShader(shaderProgram, fragmentShader); // attatches the fragment shader to the shader program
+	glLinkProgram(shaderProgram);                  // links the two shaders together 
+
+	// linking Checker / debugger
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::LINKING::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// clean up the shader data
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	// triangle data ??
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	// setup the vertex data
+
+	unsigned int VBO, VAO;
+
+	glGenVertexArrays(1, &VAO); // Vertex Array Object
+	glGenBuffers(1, &VBO); // Vertex Buffer Object
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // used to bind a buffer obejct to a buffer type 
+	glBufferData(		   // Copies the defined buffer iinto buffer memory.
+		GL_ARRAY_BUFFER,   // The type of buffer
+		sizeof(vertices),  // The size of the data
+		vertices,		   // The data we wish to send
+		GL_STATIC_DRAW	   // How we wish for the GPU to handle the data, STATIC_DRAW means that we set it once but use it multiple times
+	);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // read it on learnopengl.com I aint typing all that out
+	glEnableVertexAttribArray(0); // 
 
 	// render loop
 
@@ -50,8 +150,12 @@ int main()
 		// input
 		processInput(window);
 
-		glClearColor(0.5f, 0.0f, 1.0f, 1.0f);
+		glClearColor(0.3608f, 0.1373f, 0.9686f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram); // use the shaders linked above
+		glBindVertexArray(VAO); // sets the vertex array (still dunno what his means in human terms)
+		glDrawArrays(GL_TRIANGLES, 0, 3); // render a primitive triangle using the built in Draw Array function :D
 
 		// check and call events 
 		glfwSwapBuffers(window);
